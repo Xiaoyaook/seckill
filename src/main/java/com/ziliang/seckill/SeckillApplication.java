f0f406b92cc6faa33288aacef1ab848a9dac969e
xiaoyaook <599319698@qq.com>
2018-05-20 14:57:36 +0800

    环境搭建 thymleaf整合

---
+++ src/main/java/com/ziliang/seckill/SeckillApplication.java
@@ -0,0 +1,12 @@
+package com.ziliang.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// 启动类
+@SpringBootApplication
+public class SeckillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeckillApplication.class, args);
+	}
+}
