ceaf01d8b5b5ad0e80e8924f8e5d781a2ce93b3c
xiaoyaook <599319698@qq.com>
2018-05-21 21:12:53 +0800

    配置redis

--- src/main/java/com/ziliang/seckill/controller/DemoController.java
+++ src/main/java/com/ziliang/seckill/controller/DemoController.java
@@ -1,7 +1,12 @@
 package com.ziliang.seckill.controller;
 
+import com.ziliang.seckill.domain.User;
+import com.ziliang.seckill.redis.RedisService;
+import com.ziliang.seckill.redis.UserKey;
 import com.ziliang.seckill.result.CodeMsg;
 import com.ziliang.seckill.result.Result;
+import com.ziliang.seckill.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +15,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/demo")
 public class DemoController {
+
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
+    UserService userService;
+
     @RequestMapping("/")
     @ResponseBody
     String home() {
@@ -32,7 +44,38 @@ public class DemoController {
 
     @RequestMapping("/thymeleaf")
     public String  thymeleaf(Model model) {
-        model.addAttribute("name", "Joshua");
+        model.addAttribute("name", "xiaoyaook");
         return "hello";
+    }
+
+    @RequestMapping("/db/get")
+    @ResponseBody
+    public Result<User> dbGet() {
+        User user = userService.getById(1);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/db/tx")
+    @ResponseBody
+    public Result<Boolean> dbTx() {
+        userService.tx();
+        return Result.success(true);
+    }
+
+    @RequestMapping("/redis/get")
+    @ResponseBody
+    public Result<User> redisGet() {
+        User  user  = redisService.get(UserKey.getById, ""+1, User.class);
+        return Result.success(user);
+    }
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public Result<Boolean> redisSet() {
+        User user  = new User();
+        user.setId(1);
+        user.setName("1111");
+        redisService.set(UserKey.getById, ""+1, user);//UserKey:id1
+        return Result.success(true);
     }
 }
---
+++ src/main/java/com/ziliang/seckill/redis/KeyPrefix.java
@@ -0,0 +1,7 @@
+package com.ziliang.seckill.redis;
+
+public interface KeyPrefix {
+    public int expireSeconds();
+
+    public String getPrefix();
+}
